26c6fcfb28b9276e2be464d5e43d13a9f0abe7dc
Mitchell Hynes <mshynes@mun.ca>
2021-01-26 20:38:55 -0330

    Frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="ChatBox">
+        <pre className="ChatOutput">
+          hello
+          <br />
+          hello
+          <br />
+          hello
+          <br />
+          hello
+          <br />
+          hello
+          <br />
+        </pre>
+        <input className="ChatInput" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
